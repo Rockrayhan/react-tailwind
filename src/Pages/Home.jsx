93c02c0aa5efc843d2a966468f3eae76c81bba15
rockrayhan <khayrulalamdict@gmail.com>
2023-11-29 07:42:00 +0600

    asd

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-center'>
+            <h1 className='text-2xl'> Thsi is Home Page </h1>
+           <div className='flex gap-3 container'>
+           <div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quo debitis animi a atque sint, dolor ea voluptate quas quos ducimus vitae ipsum, similique assumenda? Maiores sunt numquam ab vitae!</p>
+            </div>
+            <div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quo debitis animi a atque sint, dolor ea voluptate quas quos ducimus vitae ipsum, similique assumenda? Maiores sunt numquam ab vitae!</p>
+            </div>
+            <div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quo debitis animi a atque sint, dolor ea voluptate quas quos ducimus vitae ipsum, similique assumenda? Maiores sunt numquam ab vitae!</p>
+            </div>
+            <div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quo debitis animi a atque sint, dolor ea voluptate quas quos ducimus vitae ipsum, similique assumenda? Maiores sunt numquam ab vitae!</p>
+            </div>
+           </div>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,48 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Pages/Home.jsx';
+import About from './Pages/About.jsx';
+import Blog from './Pages/Blog.jsx';
+import Contact from './Pages/Contact.jsx';
+import Courses from './Pages/Courses.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path:"",
+        element: <Home/>
+      },
+      {
+        path:"about",
+        element: <About/>
+      },
+      {
+        path:"blog",
+        element: <Blog/>
+      },
+      {
+        path:"contact",
+        element: <Contact/>
+      },
+      {
+        path:"courses",
+        element: <Courses/>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
