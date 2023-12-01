ba198e356e55a56f8709c612ca8c7dd7e65dccee
rockrayhan <khayrulalamdict@gmail.com>
2023-12-01 16:13:05 +0600

    countup  done

--- src/Components/AboutCounter.jsx
+++ src/Components/AboutCounter.jsx
@@ -1,32 +1,69 @@
-import React, { useEffect }  from 'react';
+import React from 'react';
 import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 
 
 const AboutCounter = () => {
 
-    useEffect(() => {
-        // This code will run when the component mounts
-        // You can add logic here to trigger the count-up animation
-      }, []);
 
 
 
     return (
         <div>
-                   <div className='flex justify-center items-center about-counter'>
-           <div className='text-white bg-teal-600 px-20 py-10  rounded-2xl	'>
-            <div className='grid grid-cols-1 lg:grid-cols-4 gap-10 justify-center'>
-                <div className="col-span-1 lg:col-span-1">
-                    <h1 className='text-5xl font-semibold'>
-                    <CountUp start={0} end={1500} duration={2} />  +
-                        </h1> 
-                    <p> Students </p> </div>
-                <div className="col-span-1 lg:col-span-1"><h1 className='text-5xl font-semibold'>30+</h1> <p> Courses </p></div>
-                <div className="col-span-1 lg:col-span-1"><h1 className='text-5xl font-semibold'>900+</h1> <p> Finished Seasons </p> </div>
-                <div className="col-span-1 lg:col-span-1"><h1 className='text-5xl font-semibold'>100%</h1> <p> Satisfaction Rate </p></div>
+            <div className='flex justify-center items-center about-counter'>
+                <div className='text-white bg-teal-600 px-20 py-10 rounded-2xl'>
+                    <div className='grid grid-cols-1 lg:grid-cols-4 gap-10 justify-center'>
+                        <div className="col-span-1 lg:col-span-1">
+                            <h1 className='text-5xl font-semibold'>
+                                <CountUp end={1500} duration={3} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>+
+                            </h1>
+                            <p> Students </p>
+                        </div>
+                        <div className="col-span-1 lg:col-span-1">
+                            <h1 className='text-5xl font-semibold'>
+                                <CountUp end={30} duration={3} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>+
+                            </h1>
+                            <p> Courses </p>
+                        </div>
+                        <div className="col-span-1 lg:col-span-1">
+                            <h1 className='text-5xl font-semibold'>
+                                <CountUp end={900} duration={3} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>+
+                            </h1>
+                            <p> Finished Seasons </p>
+                        </div>
+                        <div className="col-span-1 lg:col-span-1">
+                            <h1 className='text-5xl font-semibold'>
+                                <CountUp end={100} duration={3} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>%
+                            </h1>
+                            <p> Satisfaction Rate </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-           </div>
         </div>
     );
 };
