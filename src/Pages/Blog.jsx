977a80d67b5bb63e4efee847ae534f7bf07d99fb
rockRayhan <khayrulalamdict@gmail.com>
2023-12-17 14:30:42 +0600

    ola

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Breadcrumb from '../Components/Breadcum';
 
 const Blog = () => {
-    return (
-        <div>
-          <h1> Blog Page </h1>  
+  return (
+    <div>
+
+      {/* ==== page Header ====*/}
+      <div className='page-header'>
+        <div className='center container'>
+          <h1 className='page-header-title'> Our Blogs </h1>
+          <div>
+            <Breadcrumb></Breadcrumb>
+          </div>
         </div>
-    );
+      </div>
+      {/* ================= */}
+
+      <div className='container'>
+        
+      </div>
+
+    </div>
+  );
 };
 
 export default Blog;
